5e16e7a899a314aab3f8fcba61b73b1dbd5926c6
SkullCrush <dkaufman@college.harvard.edu>
2013-12-02 01:51:11 -0500

    This is now the HTML for the mapping page

--- public/buy.php
+++ public/buy.php
@@ -1,54 +1,44 @@
-<?php
+<!DOCTYPE html>
 
-    // configuration
-    require("../includes/config.php");
-
-    // if form was submitted
-    if ($_SERVER["REQUEST_METHOD"] == "POST")
-    {
-        // makes sure shares the user input is a positive int
-        if (!preg_match("/^\d+$/", $_POST["shares"]))
+<html>
+<head>
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet"/>
+    <link href="css/decide.css" rel="stylesheet"/>
+    <title> DecideWell </title>
+    <script>
+        function myFunction()
         {
-            apologize("You must provide a valid share amount.");
+            var node=document.createElement("div");
+            var textnode=document.createTextNode("new thing");
+            node.appendChild(textnode);
+            document.getElementById("otext").appendChild(node);
         }
-    
-        // makes sure the user input a symbol
-        if (empty($_POST["symbol"]))
-        {
-            apologize("You must provide a stock symbol.");
-        }
-        
-        // looks up stock data from Yahoo
-        $stock = lookup($_POST["symbol"]);
-        
-        // gets user's cash amount
-        $cash = query("SELECT cash FROM users WHERE id = ?", $_SESSION["id"]);
-        
-        // makes sure user has enough money to buy the requested stock
-        if ($stock["price"] * $_POST["shares"] >= $cash[0]["cash"])
-        {
-            apologize("You do not have enough funds to buy this stock.");
-        }
-        
-        // adds the stock shares to the user's portfolio
-        query("INSERT INTO portfolios (id, symbol, shares) VALUES(?, ?, ?) ON DUPLICATE KEY UPDATE shares = shares + VALUES(shares)", 
-            $_SESSION["id"], $_POST["symbol"], $_POST["shares"]);
-        
-        // charges the user for the shares
-        query("UPDATE users SET cash = cash - ? WHERE id = ?", $stock["price"] * $_POST["shares"], $_SESSION["id"]);
-        
-        // updates the history table
-        query("INSERT INTO history(id, transaction, time, symbol, shares, price) VALUES(?, 'BUY', NOW(), ?, ?, ?)", 
-            $_SESSION["id"], $_POST["symbol"], $_POST["shares"], $stock["price"]);
-        
-        // redirect to portfolio
-        redirect("/");
-    }
-    
-    else
-    {
-        // else render form
-        render("buy_form.php", ["title" => "Buy Stock"]);
-    }
-
-?>
+    </script>
+<body>
+    <div class="container">   
+        <div id = top>
+            <a href="/"><img alt="cute cat" src="cat.jpg" width="100" height="80"/></a>
+        </div>
+        <div id = left> 
+            <div id = menu1 class=menu>
+                    <button type="button" onclick="myFunction()">Click Me!</button>
+            </div>
+            <div id = menu2 class=menu>
+                will go
+            </div>
+            <div id = menu3 class=menu>
+                here
+            </div> 
+        </div>
+        <div id = middle>
+            <div id= oval>
+                <div id= otext>
+                    <div> First decision. What should I do? OMG.  </div>
+                </div>
+            </div> 
+        </div>   
+        <div id = bottom> Copyright etc. </div>
+    </div>
+</body>
+</html>
